f1c1c2588a0b545d95ce542ef6c737e3fb28a20a
Rinkore <92674395+Rinkore@users.noreply.github.com>
2023-06-02 15:01:32 +0800

    Log类第一次提交

--- src/main/java/com/github/rinkore1/iiyokoiyo/IiyokoiyoApplication.java
+++ src/main/java/com/github/rinkore1/iiyokoiyo/IiyokoiyoApplication.java
@@ -2,11 +2,19 @@ package com.github.rinkore1.iiyokoiyo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.github.rinkore1.iiyokoiyo.util.Log;
+
+import java.io.IOException;
+import java.nio.file.Paths;
 
 @SpringBootApplication
 public class IiyokoiyoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Log.setApplicationLogFile(Paths.get(System.getProperty("user.dir")));
+        Log.info("log测试%s","114514");
+        Log.debug("debug测试%s","一一四五一四");
+        Log.warning("warning测试%s","いいよ来いよ");
         SpringApplication.run(IiyokoiyoApplication.class, args);
     }
 

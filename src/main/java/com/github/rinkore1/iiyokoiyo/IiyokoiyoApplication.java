a06bd4b4209e9eaef22c450bda2102e5d4209a60
Rinkore <92674395+Rinkore@users.noreply.github.com>
2023-06-02 10:51:24 +0800

    first comit

---
+++ src/main/java/com/github/rinkore1/iiyokoiyo/IiyokoiyoApplication.java
@@ -0,0 +1,13 @@
+package com.github.rinkore1.iiyokoiyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IiyokoiyoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IiyokoiyoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/rinkore1/iiyokoiyo/IiyokoiyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rinkore1.iiyokoiyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IiyokoiyoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
